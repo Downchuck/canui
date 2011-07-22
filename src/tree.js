2380cfd770b70690c5d720cba25157229c6246b9
Jonathan McDougall <jonathanmcdougall@gmail.com>
2011-07-22 17:56:23 +0000

    tree: hit test, toggling

--- src/tree.js
+++ src/tree.js
@@ -2,12 +2,23 @@
 
 namespace("ui", {
 
+tree_parts:
+{
+  above:    0x01,
+  below:    0x02,
+  before:   0x04,
+  toggle:   0x08,
+  caption:  0x10,
+  after:    0x20
+},
+
 tree_node: function(caption)
 {
   var self = this;
 
   var tree_ = undefined;
   var caption_ = caption;
+  var expanded_ = false;
   var children_ = [];
 
   self.caption = function(c)
@@ -22,17 +33,36 @@ tree_node: function(caption)
     return caption_;
   };
 
+  self.expanded = function(b)
+  {
+    if (b != undefined)
+      expanded_ = b;
+
+    return expanded_;
+  };
+
+  self.toggle = function()
+  {
+    return self.expanded(!expanded_);
+  };
+
   self.add = function(n)
   {
     children_.push(n);
   };
 
-  self.child_count = function()
+  self.node = function(i)
+  {
+    assert(i >= 0 && i < children_.length);
+    return children_[i];
+  };
+
+  self.node_count = function()
   {
     return children_.length;
   }
 
-  self.each_child = function(f)
+  self.each_node = function(f)
   {
     for (var i in children_)
       f(children_[i]);
@@ -46,6 +76,7 @@ inherit_tree: function(self, opts)
   var root_ = new ui.tree_node("root");
   var minus_ = undefined;
   var plus_ = undefined;
+  var hovered_ = undefined;
 
   var init = function()
   {
@@ -81,6 +112,8 @@ inherit_tree: function(self, opts)
           
       root_.add(n);
     }
+
+    root_.expanded(true);
   };
 
   self.tree__best_dimension = function()
@@ -108,33 +141,99 @@ inherit_tree: function(self, opts)
 
     p.x += indents.length * self.option("indent");
 
-    var has_child = node.child_count() > 0;
+    var has_child = node.node_count() > 0;
     var sign_y = p.y + self.option("item_height")/2 - minus_.height()/2;
 
     if (has_child)
     {
-      draw_image(context, minus_, new rectangle(
-        p.x, sign_y, 0, 0));
+      if (node.expanded())
+      {
+        draw_image(context, minus_, new rectangle(
+          p.x, sign_y, 0, 0));
+      }
+      else
+      {
+        draw_image(context, plus_, new rectangle(
+          p.x, sign_y, 0, 0));
+      }
     }
+
+    draw_lines(context, clone(op), clone(p), has_child, last_child, indents);
+
+    p.x += self.option("padding");
+
+    var tc = ui.theme.text_color();
+    var tr = new rectangle(
+      p.x, p.y + self.option("item_height")/2 - g_line_height/2,
+      text_dimension(node.caption(), self.font()).w, g_line_height);
+    
+    if (node == hovered_)
+    {
+      fill_rect(context, ui.theme.selected_text_background(), tr);
+      tc = ui.theme.selected_text_color();
+    }
+
+    draw_text(context, node.caption(), tc, tr, self.font());
+
+    if (node.expanded())
+    {
+      var ci = 0;
+      node.each_node(function(n)
+        {
+          var first_child = (ci == 0);
+          var last_child = (ci == (node.node_count() - 1));
+  
+          if (last_child)
+            indents.push("none");
+          else if (node.node_count() > 0)
+            indents.push("with-sign");
+          else
+            indents.push("full");
+
+          op.y += self.option("item_height");
+          draw_node(context, op, n, indents, first_child, last_child);
+
+          indents.pop();
+
+          ++ci;
+        });
+    }
+
+    p.x -= self.option("indent");
+  };
+
+  var draw_lines = function(context, op, p, has_child, last_child, indents)
+  {
+    // 'toggle' is the plus/minus image
 
     for (var i=0; i<indents.length; ++i)
     {
-      var x = op.x + ((i + 1) * self.option("indent")) + minus_.width()/2;
+      // in the center of the toggle
+      var x =
+        op.x + ((i + 1) * self.option("indent")) + minus_.width()/2;
+
       var y=0, h=0;
 
-      var last = (indents.length - 1);
+      if (i == (indents.length - 1))
+      { 
+        // last line, the one next to the current node
 
-      if (i == last)
-      {
         if (has_child)
         {
+          // line will start right under the toggle
+          var sign_y =
+            p.y + self.option("item_height")/2 - minus_.height()/2;
+
           y = p.y;
           h = sign_y - y;
         }
         else
         {
+          // line will start at the top of the item
           y = p.y;
 
+          // the line will have the height of item, unless this is the
+          // last item, in which case the line will stop in the middle
           if (last_child)
             h = self.option("item_height") / 2;
           else
@@ -143,52 +242,134 @@ inherit_tree: function(self, opts)
       }
       else
       {
-        if (indents[i] == 1)
+        // an indent for a parent node
+
+        if (indents[i] == "full")
         {
+          // a full indent is the height of the item
           y = p.y;
           h = self.option("item_height");
         }
-        else if (indents[i] == 2)
+        else if (indents[i] == "with-sign")
         {
-          var sy = self.option("item_height")/2 + minus_.height()/2;
+          // a 'with-sign' indent tells that the preceeding node
+          // has a toggle, in which case the line needs to start a bit
+          // higher than this node so it is right under the toggle
+
+          // toggle y
+          var sy = self.option("item_height")/2 - minus_.height()/2;
+
           y = p.y - sy;
           h = self.option("item_height") + sy;
-          indents[i] = 1;
+
+          // the remaining nodes must display regular lines
+          indents[i] = "full";
         }
       }
 
       draw_dotted_line(context, new color().black(),
         new rectangle(x, y, 1, h));
     }
+  };
 
-    p.x += self.option("padding");
+  self.on_mouse_move = function(mp)
+  {
+    self.control__on_mouse_move(mp);
 
-    draw_text(context, node.caption(), ui.theme.text_color(),
-      new rectangle(p.x, p.y + self.option("item_height")/2 - g_line_height/2, 1000, 1000),
-      self.font());
+    var ht = self.hit_test(mp);
 
-    var ci = 0;
-    node.each_child(function(n)
+    hovered_ = ht.node;
+    self.redraw();
+  };
+
+  self.on_mouse_left_down = function(mp)
+  {
+    self.control__on_mouse_left_down(mp);
+    
+    var ht = self.hit_test(mp);
+    if (ht.part & ui.tree_parts.toggle)
+    {
+      ht.node.toggle();
+      self.redraw();
+    }
+  };
+
+  self.hit_test = function(p)
+  {
+    var ht = {node: undefined, indent: -1, part: 0};
+
+    if (p.y < self.option("margin"))
+    {
+      ht.part |= ui.tree_parts.above;
+    }
+    else
+    {
+      var n = find_node_by_y(p.y, root_, new point(0, self.option("margin")), 0);
+      if (n == undefined)
       {
-        var first_child = (ci == 0);
-        var last_child = (ci == (node.child_count() - 1));
-  
-        if (first_child)
-          indents.push(2);
-        else if (last_child)
-          indents.push(0);
+        ht.part |= ui.tree_parts.below;
+      }
+      else
+      {
+        ht.node = n.node;
+        ht.indent = n.indent;
+      }
+    }
+
+    if (ht.node != undefined)
+    {
+      var ix =
+        self.option("margin") + ht.indent * self.option("indent");
+
+      if (p.x < ix)
+      {
+        ht.part |= ui.tree_parts.before;
+      }
+      else
+      {
+        ix += minus_.width();
+        if (p.x < ix)
+        {
+          ht.part |= ui.tree_parts.toggle;
+        }
         else
-          indents.push(1);
+        {
+          ix += text_dimension(ht.node.caption(), self.font()).w;
+          if (p.x < ix)
+          {
+            ht.part |= ui.tree_parts.caption;
+          }
+          else
+          {
+            ht.part |= ui.tree_parts.after;
+          }
+        }
+      }
+    }
 
-        op.y += self.option("item_height");
-        draw_node(context, op, n, indents, first_child, last_child);
+    return ht;
+  };
 
-        indents.pop();
+  var find_node_by_y = function(y, node, p, indent)
+  {
+    p.y += self.option("item_height");
 
-        ++ci;
-      });
+    if (y < p.y)
+      return {node: node, indent: indent};
 
-    p.x -= self.option("indent");
+    if (node.expanded())
+    {
+      for (var i=0; i<node.node_count(); ++i)
+      {
+        var n = node.node(i);
+
+        var r = find_node_by_y(y, n, p, indent + 1);
+        if (r != undefined)
+          return r;
+      }
+    }
+
+    return undefined;
   };
 
 
